3c5b8be8216bd227c7029a53ee4984b205447a36
BorisCxD <BorisCxD@users.noreply.github.com>
2018-10-20 13:42:33 -0400

    Added header file

---
+++ Data.h
@@ -0,0 +1,26 @@
+//class definition
+
+#ifndef DATA_H
+#define DATA_H
+
+class Data
+
+{
+
+	private:
+	// attributes
+	// properties
+
+	int x;
+	float y;
+
+	public:
+		Data ();
+	
+		void setY (float a);
+			Data (int a, float b;)
+			int getX ();
+			float getY ();
+
+		void setX (int a);
+};
---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+ using namespace std;
+
+int main ()
+{
+
+vector <int> v1;
+vector <int> v2 = {5, 8, 22};
+v1 push_back (12);
+v2 push_back (17);
+v2 pop_back ();
+
+cout << " v1 has " << v2.size () << " elements ";
+
+	for ( int x = 0; ie v2.size () i++	// to process all elements
+		{
+			cout << v [i] << endl;  // to process all elements
+	for (auto X : v2)
+		{
+
+		cout << x << endl;
+
+		}
+Data d0;
+Data d1 (7, 24.83);
+
+d0.setX (15);
+d1.setY (3,14);
+
+return 0;
+
+
+
+
+}
